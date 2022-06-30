a404398473087fce5c7c08fd23c9e51b88ba8f52
Stephen Obialor <stevobialor@gmail.com>
2022-06-30 13:22:13 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,8 @@
 /* program to print out string to stdout */
 int main(void)
 {
-	/* initializing ch as strings */
-	char ch = "\"Programming is like building a multilingual puzzle";
-
-	/* write out to stdout */
-	puts(ch);
+	/* print to stdout */
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
