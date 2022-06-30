6eeeea8b254c2d4741955dabd264e7b99a5620d7
Stephen Obialor <stevobialor@gmail.com>
2022-06-30 15:05:54 -0700

    print out error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Enter here
+ *
+ * Description: Print to standard error
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful");
+	fprintf(stderr,	"- Dora Korpar, 2015-10-19\n");
+	return (0);
+}
