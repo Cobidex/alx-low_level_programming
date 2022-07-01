89372cf7c873193c440a0394f87be2b759e95033
Stephen Obialor <stevobialor@gmail.com>
2022-07-01 12:11:06 -0700

    output lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,18 @@
 /**
  * main - print out alphabet
  *
- * Description: print an alphabet p using putchar only
+ * Description: print lower case alphabet
+ * using putcharand for loop
  *
  * Return: set to 0
  */
 int main(void)
 {
-	char p = 'p';
+	int a, z;
 
-	putchar(p);
+	a = 97;
+	z = 122;
+	for (a = 97; a <= 122; a++)
+	putchar(a);
 	return (0);
 }
