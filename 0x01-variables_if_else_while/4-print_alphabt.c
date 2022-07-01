558e2baa2e335395553b7b53016d6e5de56ca15f
Stephen Obialor <stevobialor@gmail.com>
2022-07-01 13:28:13 -0700

    print and exclude e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all lower case alphabet without q and e
+ *
+ * Description: print out all lower case and exclude q and e
+ * using bolean
+ *
+ * Return: set to 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 97;
+	while (a < 123 && a != 101 && a != 113)
+	{
+		a += 1;
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
