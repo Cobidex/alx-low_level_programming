927c21bc23d9ecc6ecd35d5b131de91a7ff57e62
Stephen Obialor <stevobialor@gmail.com>
2022-07-01 14:29:35 -0700

    done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,16 @@
  */
 int main(void)
 {
-	int a;
+	int a = 97;
 
-	a = 97;
-	while (a < 123 && a != 101 && a != 113)
+	while (a < 123)
 	{
-		a += 1;
+		if (a != 101 && a != 113)
+		{
 		putchar(a);
+		a += 1;
+		}
+		putchar(10);
 	}
-	putchar(10);
 	return (0);
 }
