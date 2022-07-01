fdc28e694787599c04a7c8b08d9aab0b32503601
Stephen Obialor <stevobialor@gmail.com>
2022-07-01 11:40:08 -0700

    print alphabet with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print out alphabet
+ *
+ * Description: print an alphabet p using putchar only
+ *
+ * Return: set to 0
+ */
+int main(void)
+{
+	char p = 'p';
+
+	putchar(p);
+	return (0);
+}
