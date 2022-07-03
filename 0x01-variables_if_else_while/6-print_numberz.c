7f39f735e1da620f04bf1307a8e53f6ca52f031d
Stephen Obialor <stevobialor@gmail.com>
2022-07-02 23:24:06 -0700

    print using only putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,23 +4,19 @@
  * main - print single digit numbers
  *
  *
- * Description: print single digit numbers
+ * Description: print single digit numbers using putchar
  *
  *
- * Return: Always 0
+ * Return: Always 0 if success
  */
 
 int main(void)
 {
-	int i;
+	int a;
 
-	i = 48;
-
-	while (i < 58)
-	{
-		putchar(i);
-		i++;
-	}
+	a = 48;
+	for (a = 48; a < 58; a++)
+		putchar(a);
 	putchar(10);
 	return (0);
 }
