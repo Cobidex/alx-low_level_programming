3150c7612d5c19cf63980f453863896e3631edc8
Stephen Obialor <stevobialor@gmail.com>
2022-07-04 11:27:49 -0700

    correct

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,7 +26,7 @@ int main(void)
 					putchar(32);
 					putchar(c);
 					putchar(d);
-					if ((a < 57) || (b < 56))
+					if (a < 57 || b < 56 || c < 57 || d < 57)
 					{
 						putchar(44);
 						putchar(32);
