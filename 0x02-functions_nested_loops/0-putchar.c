fe017377b104f4203c08d55da1573efe57e859ae
Stephen Obialor <stevobialor@gmail.com>
2022-07-05 01:14:47 -0700

    print string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - output a string
+ *
+ * Description: use printf function to output string
+ *
+ * Return: set to 0 if success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
