d90aa198159e2f5b57ded5e8178cf4d558641bb3
Stephen Obialor <stevobialor@gmail.com>
2022-07-05 04:31:40 -0700

    print out lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+
+/**
+ * main - begin here
+ *
+ * Description: outputs above function
+ *
+ * Return: set to 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - print out lower case
+ *
+ * Description: print using while loop
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	a = 97;
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+}
