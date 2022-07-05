8a708595e27632bd958200d196281fcbb08da2f8
Stephen Obialor <stevobialor@gmail.com>
2022-07-05 12:21:04 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute
+ * @a: number
+ *
+ * Return: return absolute value
+ */
+int _abs(a)
+{
+	if (a < 0)
+		return (-a);
+	else if (a > 0)
+		return (a);
+	else
+		return (a);
+}
