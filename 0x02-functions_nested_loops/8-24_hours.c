99d0860f95002e45f82b0e63e2771b04e6f0e427
Stephen Obialor <stevobialor@gmail.com>
2022-07-05 22:32:42 -0700

    print 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every second of every day
+ *
+ * Return: set to zero if success
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	a = 48;
+	while (a < 51)
+	{
+		b = 48;
+		while (b < 52)
+		{
+			c = 48;
+			while (c < 54)
+			{
+				d = 48;
+				while (d < 58)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
