544f6b98fc1f2419a48e348c52db60eb175f9275
Stephen Obialor <stevobialor@gmail.com>
2022-07-07 17:44:21 +0000

    done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,10 @@ void print_most_numbers(void)
 	a = 48;
 	for (a = 48; a < 58; a++)
 	{
-		if (a != 50 || a != 52)
-		{
+		if (a == 50 || a == 52)
+			a++
+		else
 			_putchar(a);
-		}
 	}
 	_putchar(10);
 }
