3674f732487b0b4886e9f4daec360d7145ead72b
Stephen Obialor <stevobialor@gmail.com>
2022-07-07 15:47:51 +0000

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all single integers
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	a = 48;
+	for (a = 48, a < 58, a++)
+		_putchar(a)
+}
