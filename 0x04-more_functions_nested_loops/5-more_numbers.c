a491490b2d5a08973e554a65045fac90b8836dff
Stephen Obialor <stevobialor@gmail.com>
2022-07-07 18:50:45 +0000

    done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+	for (a = 0; a < 11; a++)
+	{
+		for (b = 0; b < 15; b++)
+			_putchar(b + '0');
+		_putchar(10);
+	}
+	_putchar(10);
+}
