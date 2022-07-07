d88af5a59882b0087ad938dfa4bf22b9d8bfd164
Stephen Obialor <stevobialor@gmail.com>
2022-07-07 21:38:19 +0000

    done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints out line
+ * @n: integer
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 1; a <= n; a++)
+			_putchar('_');
+		_putchar(10);
+	}
+	else
+		_putchar(10);
+}
