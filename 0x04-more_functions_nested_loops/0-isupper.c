eb95c81b5ebb17a1b3e7942f3c9736d3f2f938a6
Stephen Obialor <stevobialor@gmail.com>
2022-07-07 15:28:26 +0000

    check

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * _isupper - checks if upper case
+ * @c: character
+ * Return: set to zerp
+ */
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
