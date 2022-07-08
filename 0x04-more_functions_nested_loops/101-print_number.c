5fe2e17d763c75270f787397045b949ab9061e07
Stephen Obialor <stevobialor@gmail.com>
2022-07-08 21:42:53 +0000

    done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_number - prints any integer
+ * @n: integer
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 10 && n > 0)
+		_putchar(n % 10 + '0');
+	if (n > 9 && n < 100)
+	{
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	if (n > 99 && n < 1000)
+	{
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	if (n > 999 && n < 10000)
+	{
+		_putchar((n / 1000) % 10 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	if (n < 0 && n > -10)
+	{
+		_putchar('-');
+		_putchar((-n % 10) + '0');
+	}
+	if (n < -9 && n > -100)
+	{
+		_putchar('-');
+		_putchar(((-n / 10) % 10) + '0');
+		_putchar((-n % 10) + '0');
+	}
+}
