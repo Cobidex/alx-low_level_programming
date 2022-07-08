f17e8bb0665efe21933d57587b15ac8873a74ac6
Stephen Obialor <stevobialor@gmail.com>
2022-07-08 12:08:23 +0000

    done

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,9 @@ void print_triangle(int size)
 	{
 		for (a = 1; a <= size; a++)
 		{
-			for (b = 1; b < (size - a); b++)
+			for (b = 1; b <= (size - a); b++)
 				_putchar(' ');
-			for (c = 1; b <= a; c++)
+			for (c = 1; c <= a; c++)
 				_putchar('#');
 			_putchar('\n');
 		}
