9a737ae9fc8c1a858100890338f88edd77affd71
Stephen Obialor <stevobialor@gmail.com>
2022-07-12 19:18:06 +0000

    done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints out
+ * @str: string
+ * @*str: string pointer
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int a, s;
+
+	for (a = 0; a != '\0'; a++)
+	{
+		s = *(str + a);
+		_putchar(s);
+	}
+}
