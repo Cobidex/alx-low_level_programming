581d13cc9f895c93a34140b42a199f5a738ddfa6
Stephen Obialor <stevobialor@gmail.com>
2022-07-14 17:01:55 +0000

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,22 +4,31 @@
  * _strcat - concatenate strings
  * @dest: pointer 1
  * @src: pointer 2
- * Return: char
+ * Return: o if successful
  */
 
 char *_strcat(char *dest, char *src)
 {
-	char *new = dest;
-	char *d;
-	int a, c;
+	char *new;
+	int a, b, c;
 
 	a = 0;
+	b = 0;
 	c = 0;
-	d = new + c;
-	for (new = new; *new = '\0'; new++)
-		c++;
-	for (d = src; *src = '\0'; d++)
+	while (*dest != '\0')
+	{
+		new = (new + a);
 		a++;
-	dest = new;
-	return (dest);
+	}
+	for (src = src; *src = '\0'; src++)
+	{
+		*(new + b) = *(src + b);
+		b++;
+	}
+	for (new = dest; new = '\0'; new++)
+	{
+		*(dest + c) = *(new + c);
+		c++;
+	}
+	return (0);
 }
