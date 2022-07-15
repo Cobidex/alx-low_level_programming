5c209cef4ec77767272c9660afe4ec9aef8c826f
Stephen Obialor <stevobialor@gmail.com>
2022-07-15 18:59:41 +0000

    done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - upper case
+ * *: pointer
+ * Return: char
+ */
+
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+		a++;
+	}
+	return (p);
+}
