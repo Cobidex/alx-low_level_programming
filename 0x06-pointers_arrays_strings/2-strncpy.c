88681b4784d7d9ae519cc3a4eecdfd7f293c5eff
Stephen Obialor <stevobialor@gmail.com>
2022-07-15 05:41:33 +0000

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string
+ * @dest: first pointer
+ * @src: second pointer
+ * @n: integer
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+	char *temp;
+
+	a = 0;
+	temp = src;
+	while (a <= n)
+	{
+		temp[a] = src[a];
+		a++;
+	}
+	temp[a] = '\0';
+	dest = temp;
+	return (dest);
+}
