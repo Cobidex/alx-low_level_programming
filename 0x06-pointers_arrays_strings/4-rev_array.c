8fb276195def7d677a712d664a0235214e29b2e9
Stephen Obialor <stevobialor@gmail.com>
2022-07-15 18:01:37 +0000

    done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,11 +12,11 @@ void reverse_array(int *a, int n)
 	int *p, b, d, e;
 
 	p = a;
-	for (b = 0; b <= n; b++)
+	for (b = 0; b < n; b++)
 	{
 		d = n - b;
 		p[b] = *(a + d);
 	}
-	for (e = 0; e <= n; e++)
+	for (e = 0; e < n; e++)
 		*(a + e) = p[e];
 }
