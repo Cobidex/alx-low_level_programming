abb664dd6b1fb3f984ecc79728e1d2ce6ef70a31
Stephen Obialor <stevobialor@gmail.com>
2022-07-16 05:06:28 +0000

    done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * string_toupper - upper case
+ * @p: pointer
+ * Return: char
+ */
+
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+			p[a] -= 32;
+		if (p[a] == '.')
+		{
+			a++;
+			if (p[a] >= 97 && p[a] <= 122)
+				p[a] -= 32;
+		}
+		if (p[a] == ' ')
+		{
+			a++;
+			if (p[a] >= 97 && p[a] <= 122)
+				p[a] -= 32;
+		}
+		if (p[a] == '\n' || p[a] == '\t')
+		{
+			a++;
+			if (p[a] >= 97 && p[a] <= 122)
+				p[a] -= 32;
+		}
+		a++;
+	return (p);
+}
