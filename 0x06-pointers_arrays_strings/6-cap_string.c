4321f26d1e4ed0af2e835ab6a5c7ca94d3ca38fe
Stephen Obialor <stevobialor@gmail.com>
2022-07-16 11:50:53 +0000

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,42 +1,38 @@
 #include "main.h"
 
 /**
- * cap_string - upper case
- * @p: pointer
- * Return: char
+ * cap_string - capitalizes strings
+ * @p: string to capitalize
+ * Return: p
  */
 
 char *cap_string(char *p)
 {
-	int a = 0;
+	int a, b;
 
+	char spc[14] = {',', '.', ';', '!', '?', '"', '(', ')',
+	'{', '}', ' ', '\n', '\t'};
+
+
+	a = 0;
 	while (p[a])
 	{
 		if (a == 0)
 		{
 			if (p[a] >= 97 && p[a] <= 122)
 				p[a] -= 32;
-		}
-		if (p[a] == '.')
-		{
 			a++;
-			if (p[a] != ' ')
+		}
+		b = 0;
+		while (spc[b] && b < 14)
+		{
+			if (p[a] == spc[b])
 			{
+				a++;
 				if (p[a] >= 97 && p[a] <= 122)
 					p[a] -= 32;
 			}
-		}
-		if (p[a] == ' ')
-		{
-			a++;
-			if (p[a] >= 97 && p[a] <= 122)
-				p[a] -= 32;
-		}
-		if (p[a] == '\n' || p[a] == '\t')
-		{
-			a++;
-			if (p[a] >= 97 && p[a] <= 122)
-				p[a] -= 32;
+			b++;
 		}
 		a++;
 	}
