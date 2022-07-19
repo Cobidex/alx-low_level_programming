a2f5ced86b685db3cd83f762e6267f7c8fde8047
Stephen Obialor <stevobialor@gmail.com>
2022-07-19 09:01:37 +0000

    done

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates first char
+ * @s: string
+ * @accept: string chars
+ * Return: pointr or null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *p;
+	int a, b, c, d;
+
+	a = 0;
+	while (s[a])
+		a++;
+	while (accept[b])
+		b++;
+	for (c = 0; c <= a; c++)
+	{
+		for (d = 0; d <= b; d++)
+		{
+			if (s[c] == accept[d])
+			{
+				p = (s + c);
+				return (p);
+			}
+		}
+	}
+	return ('\0');
+}
