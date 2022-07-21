6f2a8091abef1b24988590f3689393fc004ad1ac
Stephen Obialor <stevobialor@gmail.com>
2022-07-21 19:54:35 +0000

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-	int n1 = atoi(argv[1]);
+	int n1 = 0;
 	int n2 = atoi(argv[2]);
 
 	if (argc == 3)
 	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
 		printf("%d\n", n1 * n2);
 	}
 	else
