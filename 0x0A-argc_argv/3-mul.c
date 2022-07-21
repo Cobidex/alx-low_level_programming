debfab3c8b87246b578eab195ce0a75b2c4fb420
Stephen Obialor <stevobialor@gmail.com>
2022-07-21 19:51:56 +0000

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,22 +6,21 @@
  * main - multiplies two numbers
  * @argc: count
  * @argv: vector
- * Return: 1
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	int n1 = atoi(argv[1]);
 	int n2 = atoi(argv[2]);
-	int product = n1 * n2;
 
 	if (argc == 3)
 	{
-		printf("%d\n", product);
+		printf("%d\n", n1 * n2);
 	}
 	else
 	{
 		printf("Error\n");
 	}
-	return (1);
+	return (0);
 }
