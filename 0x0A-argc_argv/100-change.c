ed5ca07af39bf8d4bb9642034d2faa1461b97349
Stephen Obialor <stevobialor@gmail.com>
2022-07-26 06:10:40 +0000

    done

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - prints change
+ * @argc: count
+ * @argv: vector
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int change[5] = {25, 10, 5, 2, 1};
+	int a;
+	int cents = 0;
+	int num = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("0\n");
+	}
+	for (a = 0; a < 5; a++)
+	{
+		while (change[a] <= num)
+		{
+			num -= change[a];
+			cents++;
+			if (num == 0)
+			{
+				printf("%d\n", cents);
+			}
+		}
+	}
+	return (0);
+}
