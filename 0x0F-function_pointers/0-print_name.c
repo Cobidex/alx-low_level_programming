bb3a7ac8ecc3c6379459753ba11f978ce95a9c28
Stephen Obialor <stevobialor@gmail.com>
2022-08-03 08:46:13 +0000

    Ddone

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_name - prints name to standard output
+ * @name: pointer to name
+ * @f: pointer to function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
