de3b12e97035c3932c941e3a49dd5bea8e89534a
Stephen Obialor <stevobialor@gmail.com>
2022-08-03 09:38:58 +0000

    function to check for number in array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: pointer to base element
+ * @size: number of array elements
+ * @cmp: pointer to function to compare values
+ * Return: index||0||-1||-1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index = 0;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		while (index < size)
+		{
+			if (cmp(array[index]))
+				return (index);
+			index++;
+		}
+		return (-1);
+	}
+}
