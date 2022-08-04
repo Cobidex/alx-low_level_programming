6ffac5203dfb713758bb96882c63ea9a062858e5
Stephen Obialor <stevobialor@gmail.com>
2022-08-04 14:49:42 +0000

    add comma to printf on line 26

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		p = va_arg(args, char *);
 		(!p) ? printf("nil") : printf("%s", p);
 		if (separator && i < n - 1)
-			printf("%s" separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 }
