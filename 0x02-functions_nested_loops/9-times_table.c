8760f02e56bdd222c09dff1573ee466922e59724
Stephen Obialor <stevobialor@gmail.com>
2022-08-07 16:31:00 +0000

    print n times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,38 @@
 #include "main.h"
 
-/**
- * times_table - print 9 times table
- *
- * Return: set to void
- */
 void times_table(void)
 {
-	int a;
+	int i = 0, j, product = 0;
 
-	a = 50;
-	_putchar(a);
+	while (i < 10)
+	{
+
+		while (j < 10)
+		{
+			j = 0;
+			product = i * j;
+			if (product < 10)
+			{
+				_putchar(product + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
