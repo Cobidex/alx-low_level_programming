bd28ecf7511a36d3d21d89408b7dfd381b8337ad
Stephen <stevobialor@gmail.com>
2022-09-11 08:49:39 +0000

    print first 50 fib series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints fibonnacci numbers
+ * Description: Finds the first 50 fibonacci numbers
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	unsigned int a = 1, b = 1, c, count = 1;
+
+	while (count < 50)
+	{
+		c = b;
+		b += a;
+		a = c;
+		printf("%u, ", a);
+		++count;
+	}
+	printf("%u\n", b);
+	return (0);
+}
