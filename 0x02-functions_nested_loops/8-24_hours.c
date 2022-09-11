9412e668f147bba79041570b918b2bfda0cda9f5
Stephen <stevobialor@gmail.com>
2022-09-11 08:00:49 +0000

    repair output

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,23 +9,23 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	a = 48;
-	while (a < 51)
+	a = 0;
+	while (a < 3)
 	{
-		b = 48;
-		while (b < 52)
+		b = 0;
+		while (b < 4)
 		{
-			c = 48;
-			while (c < 54)
+			c = 0;
+			while (c < 6)
 			{
-				d = 48;
-				while (d < 58)
+				d = 0;
+				while (d < 10)
 				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
 					_putchar('\n');
 					d++;
 				}
