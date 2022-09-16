734156e2eab7449912b49b443a457f6bd5374362
Stephen <stevobialor@gmail.com>
2022-09-16 19:38:39 +0000

    convert string to number

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - converts strings to numbers
+ * @s: string to be converted
+ * Return: converted number
+ */
+
+int _atoi(char *s)
+{
+	int nb = 0, i = 0, sign = 1;
+
+	while (s[i])
+	{
+		if (s[i] == '-')
+		{
+			i++;
+			if (s[i] <= '9' && s[i] >= '0')
+				sign *= -1;
+			else
+				continue;
+		}
+		if (s[i] <= '9' && s[i] >= '0')
+		{
+			nb = nb * 10 + (s[i] - '0');
+			i++;
+			if (s[i] > '9' || s[i] < '0')
+				break;
+		}
+		else
+			i++;
+	}
+	nb *= sign;
+	return (nb);
+}
