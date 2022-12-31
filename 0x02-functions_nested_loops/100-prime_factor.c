9ea6ce36bcbde029853e8f38f07100599ddf0ad2
Stephen <stevobialor@gmail.com>
2022-12-31 15:07:21 +0000

    done

---
+++ 0x02-functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - finds the maximum prime number
+ * Description: uses nested while loops
+ * Return: 0
+ */
+
+int main(void)
+{
+	long num = 612852475143, div = 2;
+
+	while (num > 1)
+	{
+		while (num % div == 0)
+			num /= div;
+		div++;
+	}
+	printf("%ld\n", --div);
+	return (0);
+}
