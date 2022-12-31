bd7f8b8c3776d251218c858cdea4a9bad1b06a69
Stephen <stevobialor@gmail.com>
2022-12-31 15:27:52 +0000

    done

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,25 +3,22 @@
 #include <time.h>
 
 /**
-
-main - Generate and print a random valid password for the program 101-crackme
-
-Return: Always 0 (Success)
+ * main - Generate and print a random valid password
+ * for the program 101-crackme
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
-	// Seed the random number generator with the current time
 	srand(time(NULL));
 
-	// Generate and print a random password
 	printf("%c%c%c%c%c%c%c\n",
-	rand() % 26 + 'a', // Random lowercase letter
-	rand() % 26 + 'A', // Random uppercase letter
-	rand() % 10 + '0', // Random digit
-	rand() % 26 + 'a', // Random lowercase letter
-	rand() % 26 + 'A', // Random uppercase letter
-	rand() % 10 + '0', // Random digit
-	rand() % 26 + 'a'); // Random lowercase letter
+	rand() % 26 + 'a',
+	rand() % 26 + 'A',
+	rand() % 10 + '0',
+	rand() % 26 + 'a',
+	rand() % 26 + 'A',
+	rand() % 10 + '0',
+	rand() % 26 + 'a');
 
-	return 0;
+	return (0);
 }
