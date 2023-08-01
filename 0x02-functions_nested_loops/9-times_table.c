9b3210ec8a1191565891ae755d5fa69378673a43
Stephen Obialor <stevobialor@gmail.com>
2023-08-01 06:59:25 +0000

    format output correctly

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,43 +1,64 @@
 #include "main.h"
 
 /**
+ * print_value - prints the value
+ * @value: to be printed
+ *
+ * Return: void
+ */
+void print_value(int value)
+{
+	if (value / 10)
+	{
+		_putchar(value / 10 + '0');
+		_putchar(value % 10 + '0');
+	}
+	else
+	{
+		_putchar(value + '0');
+	}
+}
+
+
+/**
+ * print_row - print the rows of the table
+ *
+ * @column: the current column to print
+ *
+ * Return: void
+ */
+void print_row(int column)
+{
+	int index, value;
+
+	for (index = 0; index < 10; index++)
+	{
+		value = column * index;
+		if (index != 0 && !(value / 10))
+		{
+			_putchar(' ');
+		}
+		print_value(value);
+		if (index < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
+
+/**
  * times_table - prints n times table
  * Return: void
  */
 
 void times_table(void)
 {
-	int i = 0, j, product = 0;
+	int column;
 
-	while (i < 10)
+	for (column = 0; column < 10; column++)
 	{
-		j = 0;
-		while (j < 10)
-		{
-			product = i * j;
-			if (product < 10)
-			{
-				_putchar(product + '0');
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			j++;
-		}
-		_putchar('\n');
-		i++;
+		print_row(column);
 	}
 }
